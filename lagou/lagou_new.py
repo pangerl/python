f70f6b9be23117fa2ee1bfef6483aa0aa7cffc6c
薄荷盐 <hxjagf@qq.com>
2018-05-07 15:19:20 +0800

    Update lagou_new.py

--- lagou/lagou_new.py
+++ lagou/lagou_new.py
@@ -12,7 +12,7 @@ def fetch(url, postdata=None):
     'Referer': 'https://www.lagou.com/jobs/list_%E8%BF%90%E7%BB%B4%E5%B7%A5%E7%A8%8B%E5%B8%88?px=default&city=%E6%AD%A6%E6%B1%89',
     'Host':'www.lagou.com',
     'Upgrade-Insecure-Requests': '1',
-    'Cookie': 'JSESSIONID=ABAAABAAAFCAAEG04EC8405BCC18F64C566C833E30833EF; _ga=GA1.2.604249756.1525311743; _gid=GA1.2.249895278.1525311743; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1525311744; user_trace_token=20180503094223-3a07bf5e-4e73-11e8-bd62-5254005c3644; LGUID=20180503094223-3a07c40d-4e73-11e8-bd62-5254005c3644; X_HTTP_TOKEN=a414ce312610d84d12a1fa5423f80b68; LG_LOGIN_USER_ID=9ddc8037b6c38cdd64901f065a67ea3a73b4f7938fd9fdaa; _putrc=DA1C985ED9E706CA; login=true; unick=%E9%BB%84%E5%B0%8F%E5%86%9B; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=56; index_location_city=%E6%9D%AD%E5%B7%9E; TG-TRACK-CODE=search_code; gate_login_token=476dcdd60e32a5101e12681a1ac265ed887fb2536169a7f1; LGRID=20180503140144-74cd418e-4e97-11e8-b884-525400f775ce; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1525327304; SEARCH_ID=585676fb28734fa2bced257342d2a805'
+    'Cookie': 'xxxxx'
     }
     if postdata is not None:
         return s.post(url, timeout=5, data=postdata, headers=headers)
